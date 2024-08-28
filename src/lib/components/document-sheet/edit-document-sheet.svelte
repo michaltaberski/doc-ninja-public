1f710f7b0b360ce2aa9fcad833f6d2a51b6239d1
Michał Taberski <m.taberski@gmail.com>
2024-08-28 09:49:59 +0200

    Update edit-document-sheet.svelte

--- src/lib/components/document-sheet/edit-document-sheet.svelte
+++ src/lib/components/document-sheet/edit-document-sheet.svelte
@@ -31,45 +31,10 @@
 
 <Sheet.Root {open} onOpenChange={(newState) => (open = newState)}>
   <Sheet.Content showClose={false} class="w-full sm:max-w-2xl sm:rounded-lg">
-    <Sheet.Header class="relative">
-      <div class="flex max-h-96 min-h-96 border-b">
-        {#if document}
-          {#each document?.files || [] as file}
-            <FilePreview {document} {file} />
-          {/each}
-        {/if}
-      </div>
-      <div
-        class="absolute bottom-0 left-0 right-0 z-10 flex items-center justify-between border-b bg-background/40 px-4 py-2 backdrop-blur-md"
-      >
-        <div class="flex items-center gap-2">
-          <Button size="icon" variant="outline">
-            <ChevronLeftIcon class="h-4 w-4" />
-            <span class="sr-only">Previous Page</span>
-          </Button>
-          <Button size="icon" variant="outline">
-            <ChevronRightIcon class="h-4 w-4" />
-            <span class="sr-only">Next Page</span>
-          </Button>
-          <span class="text-sm text-muted-foreground">Page 1 of 10</span>
-        </div>
-        <div class="flex items-center gap-2">
-          <!--
-          <Slider
-            class="w-24 [&>span:first-child]:h-1 [&>span:first-child]:bg-muted-foreground [&_[role=slider]]:bg-primary [&_[role=slider]]:w-3 [&_[role=slider]]:h-3 [&_[role=slider]]:border-0 [&>span:first-child_span]:bg-primary [&_[role=slider]:focus-visible]:ring-0 [&_[role=slider]:focus-visible]:ring-offset-0 [&_[role=slider]:focus-visible]:scale-105 [&_[role=slider]:focus-visible]:transition-transform"
-            defaultValue={[50]}
-            max={100}
-            min={10}
-            step={10}
-            aria-label="Zoom"
-          />
-          -->
-          <Button size="icon" variant="outline" onclick={onCancel}>
-            <DownloadIcon class="h-4 w-4" />
-            <span class="sr-only">Download PDF</span>
-          </Button>
-        </div>
-      </div>
+    <Sheet.Header>
+      {#if document?.files.length}
+        <FilePreview {document} file={document.files[0]} class="h-96 max-h-96" />
+      {/if}
     </Sheet.Header>
     <Sheet.Body>
       <div class="flex flex-col p-6">
