966b26cab400d1d28beb2884ff3d5f51f0430c99
Michał Taberski <m.taberski@gmail.com>
2024-08-29 23:35:21 +0200

    Update file-preview-local.svelte

--- src/lib/components/document-sheet/file-preview-local.svelte
+++ src/lib/components/document-sheet/file-preview-local.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { FileType } from '@/pb/types';
-  import { loadFileToSrc } from '@/lib/utils';
+  import { cn, loadFileToSrc } from '@/lib/utils';
   import FilePreviewSrc from './file-preview-src.svelte';
 
   const { file, class: className }: { file: File; class?: string } = $props();
@@ -20,6 +20,8 @@
   const srcPromise = $derived(loadFileToSrc(file));
 </script>
 
-{#await srcPromise then src}
+{#await srcPromise}
+  <div class={cn('border-b bg-gray-100', className)}></div>
+{:then src}
   <FilePreviewSrc {src} {fileType} {fileName} class={className} />
 {/await}
